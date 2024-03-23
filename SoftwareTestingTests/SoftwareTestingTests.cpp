73cd4d1a3cfb3ff566198d828d8a456cd6f7c42c
igor <igoral2080@gmail.com>
2024-03-23 09:58:06 +0200

    Initial commit

---
+++ SoftwareTestingTests/SoftwareTestingTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SoftwareTestingTests
+{
+	TEST_CLASS(SoftwareTestingTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
