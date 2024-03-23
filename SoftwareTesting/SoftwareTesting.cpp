d0d80d7d0fe60528f1463542ae200483818429e2
igor <igoral2080@gmail.com>
2024-03-23 10:08:50 +0200

    add source code for testing

--- SoftwareTesting/SoftwareTesting.cpp
+++ SoftwareTesting/SoftwareTesting.cpp
@@ -2,9 +2,75 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+static void manipulateVector(vector<int>& vec) {
+	// Check if the vector is empty
+	if (vec.empty()) {
+		cout << "Vector is empty. Nothing to manipulate." << endl;
+		return;
+	}
+
+	// Add 10 to each element of the vector
+	for (int& num : vec) {
+		num += 10;
+	}
+
+	// Remove duplicates
+	sort(vec.begin(), vec.end());
+	vec.erase(unique(vec.begin(), vec.end()), vec.end());
+
+	// Reverse the vector
+	reverse(vec.begin(), vec.end());
+
+	// Print the modified vector
+	cout << "Modified vector: ";
+	for (int num : vec) {
+		cout << num << " ";
+	}
+	cout << endl;
+
+	// Calculate the sum of the elements
+	int sum = 0;
+	for (int num : vec) {
+		sum += num;
+	}
+	cout << "Sum of the elements: " << sum << endl;
+
+	// Find the maximum element
+	int maxElement = *max_element(vec.begin(), vec.end());
+	cout << "Maximum element: " << maxElement << endl;
+
+	// Find the minimum element
+	int minElement = *min_element(vec.begin(), vec.end());
+	cout << "Minimum element: " << minElement << endl;
+
+	// Calculate the average
+	double average = static_cast<double>(sum) / vec.size();
+	cout << "Average of the elements: " << average << endl;
+
+	// Count the number of even and odd numbers
+	int numEven = 0, numOdd = 0;
+	for (int num : vec) {
+		if (num % 2 == 0) {
+			numEven++;
+		}
+		else {
+			numOdd++;
+		}
+	}
+	cout << "Number of even numbers: " << numEven << endl;
+	cout << "Number of odd numbers: " << numOdd << endl;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//example use:
+	vector<int> v;
+	v.push_back(27363627);
+	manipulateVector(v);
 }
-
